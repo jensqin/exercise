6095b91f828863181d93019977edd6b38844f965
ZQ-AWS-Py3 <zqin@bigleagueadvance.com>
2019-10-16 19:34:42 +0000

    tf: move to local machine

--- tensorflow/basics.py
+++ tensorflow/basics.py
@@ -1,2 +1,38 @@
 import pandas as pd 
 import tensorflow as tf 
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import sklearn
+import sys
+import tensorflow as tf
+from tensorflow import keras  # tf.keras
+import time
+
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = (fashion_mnist.load_data())
+X_valid, X_train = X_train_full[:5000], X_train_full[5000:]
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+               "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+# 2.1
+# model = keras.models.Sequential()
+# model.add(keras.layers.Flatten(input_shape=[28,28]))
+# model.add(keras.layers.Dense(300, activation="relu"))
+# model.add(keras.layers.Dense(100, activation="relu"))
+# model.add(keras.layers.Dense(10, activation="softmax"))
+
+model = keras.models.Sequential(
+    [
+        keras.layers.Flatten(input_shape=[28,28]),
+        keras.layers.Dense(300, activation='relu'),
+        keras.layers.Dense(100, activation='relu'),
+        keras.layers.Dense(10, activation='softmax')
+    ]
+)
+
+model.summary()
