8041aa542f53f83eb24a695b2df5415ba03c09ba
Zhen Qin <qinzhensir@outlook.com>
2020-02-27 15:45:44 -0500

    pandas-etl: creation

--- data/backtest.sql
+++
@@ -1,4 +0,0 @@
-SELECT Version, Season, Week, GameId, FranchiseId, OpponentId, PlayerId, 
-Unit, GamePosition, Role, Ratio, Status, AdjPred
-FROM Model_NFL_RoleSnap
-WHERE Season = 2019 AND Version IN (0, 1);
