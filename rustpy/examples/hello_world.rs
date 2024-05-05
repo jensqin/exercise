7806ad6d9a9950af1dae8fb660086a93f89414fa
Zhen Qin <qinzhensir@outlook.com>
2024-05-05 10:19:16 -0400

    wip: init rust

---
+++ rustpy/examples/hello_world.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+
+    let x = 3;
+
+    if x > 0 {
+        println!("greater than zero.");
+    } else {
+        println!("not greater than zero.");
+    }
+}
